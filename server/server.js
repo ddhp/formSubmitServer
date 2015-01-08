06d68fb5f94625284dc9d46dc3c208a696fe9d18
ddhp <sevenknee@gmail.com>
2015-01-09 00:16:31 +0800

    only serve static public folder

--- server/server.js
+++ server/server.js
@@ -42,10 +42,13 @@ app.get('/user', function (req, res) {
   }) 
 })
 
-server.listen(3000);
-console.log('app start listen to port 3000');
+server.listen(3000, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log('Example app listening at http://%s:%s', host, port)
+})
 
 // set base path
-baseDir = path.normalize(__dirname + "/..");
+baseDir = path.normalize(__dirname + "/../public");
 app.use(express.static(baseDir));
 
